3b1ed064a1c61c0d9497ad2ffd688f846e8545a5
Calvin Wang <calvin.wang0628@gmail.com>
2019-02-16 13:08:42 -0800

    BOW, MNB done, TFIDF in progress

---
+++ src/mnb.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import math
+from utils import *
+
+class MN_NaiveBayes:
+    def __init__(self, pos, neg):
+        self.pos_count = count(pos)
+        self.neg_count = count(neg)
+        self.doc_count = self.pos_count + self.neg_count
+        self.pos = pos
+        self.neg = neg
+
+    def train(self):
+        self.features = {}
+        self.features['posFeatures'] = {}
+        self.features['negFeatures'] = {}
+
+        # Gathering a priori probabilities by class
+        self.priorLogPos = math.log(self.pos_count / self.doc_count)
+        self.priorLogNeg = math.log(self.neg_count / self.doc_count)
+
+        """
+        Each for loop below is calculating probabilities of each feature
+        for each class.
+        Backslashes in calculations are added for readiblity and serve as 
+        line breaks.
+        """
+        for word, count in self.pos.items():
+            self.features['posFeatures'][word] = math.log((int(count) + 1) \
+                                                          / (self.pos_count + self.doc_count))
+        for word, count in self.neg.items():
+            self.features['negFeatures'][word] = math.log((int(count) + 1) \
+                                                          / (self.neg_count + self.doc_count))
+
+    """
+    Takes a given test document and make a classification decision based off
+    of a max probability.
+    @param document: Test document used to make classification decision.
+    return: A two-tuple with the classification decision and its corresponding
+    log-space probability.
+    """
+    def testHelper(self, validationSet, posTestCount, negTestCount):
+        pos_val = self.priorLogPos
+        neg_val = self.priorLogNeg
+
+        # Smoothed probabilities are calculated below, these are used when a
+        # word in the test document is not found in the given class but is found
+        # in another class's feature dict
+        smooth_pos = math.log(1 / (self.pos_count + self.doc_count))
+        smooth_neg = math.log(1 / (self.neg_count + self.doc_count))
+
+        for line in lines(data_dir + validationSet):
+            words = line.strip().split()
+            for feature in self.features:
+                if feature == 'posFeatures':
+                    for word in words:
+                        if word in self.features['posFeatures']:
+                            pos_val += self.features['posFeatures'][word]
+                        elif word in self.features['negFeatures']:
+                            pos_val += smooth_pos
+                elif feature == 'negFeatures':
+                    for word in words:
+                        if word in self.features['negFeatures']:
+                            neg_val += self.features['negFeatures'][word]
+                        elif word in self.features['posFeatures']:
+                            neg_val += smooth_neg
+
+                # print(pos_val, neg_val)
+
+                if pos_val > neg_val:
+                    posTestCount += 1
+                    # print('pos', line)
+                elif neg_val > pos_val:
+                    negTestCount += 1
+                    # print('neg', line)
+                else: # TODO: Reorg w assert before if
+                    assert(pos_val != neg_val)
+
+        return(posTestCount, negTestCount)
+
+    def test(self):
+        posTestCount, negTestCount = 0, 0
+
+        posTestCount, negTestCount = self.testHelper(filenames[2], posTestCount, negTestCount)
+        print(posTestCount,negTestCount)
+        posTestCount, negTestCount = self.testHelper(filenames[3], posTestCount, negTestCount)
+        print(posTestCount, negTestCount)
+
+        return(posTestCount, negTestCount)
---
+++ src/utils.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+from collections import Counter
+
+data_dir = "../data/"
+filenames = ["training_pos.txt",
+             "training_neg.txt",
+             "test_pos_public.txt",
+             "test_neg_public.txt"]
+
+def lines(infile):
+    with open(infile, 'r') as fp:
+        for line in fp:
+            yield line
+
+def countTrainBOW():
+    """
+    takes file and returns the counter object (word, n_appearance) of whole document
+    :param filenames:
+    :return:
+    """
+    cnt_pos = Counter()
+    cnt_neg = Counter()
+    for line in lines(data_dir + filenames[0]):
+        words = line.strip().split()
+        cnt_pos.update(words)
+
+    for line in lines(data_dir + filenames[1]):
+        words = line.strip().split()
+        cnt_neg.update(words)
+
+    # TODO: Build an overall vocab set?
+    # vocab = list(myCounter.keys())
+
+    for word in cnt_pos:
+        cnt_pos[word] += 1
+
+    for word in cnt_neg:
+        cnt_neg[word] += 1
+
+    return cnt_pos, cnt_neg
+
+def countTrainTFIDF():
+    """
+        takes file and returns the counter object (word, n_appearance) of whole document
+        ADDITIONALLY: returns a dictionary of the # reviews the term appears in
+        :param filenames:
+        :return:
+        """
+    cnt_pos = Counter()
+    cnt_neg = Counter()
+    IDF = {}
+    for line in lines(data_dir + filenames[0]):
+        words = line.strip().split()
+        cnt_pos.update(words)
+        for word in words:
+            if word in IDF:
+                IDF[word] += 1
+            else:
+                IDF[word] = 1
+
+    for line in lines(data_dir + filenames[1]):
+        words = line.strip().split()
+        cnt_neg.update(words)
+        for word in words:
+            if word in IDF:
+                IDF[word] += 1
+            else:
+                IDF[word] = 1
+
+    for word in cnt_pos:
+        cnt_pos[word] += 1
+
+    for word in cnt_neg:
+        cnt_neg[word] += 1
+
+    return cnt_pos, cnt_neg, IDF
+
+def count(pol_dict):
+    counter = 0
+    for entry in pol_dict:
+        counter += int(pol_dict[entry])
+    return counter
